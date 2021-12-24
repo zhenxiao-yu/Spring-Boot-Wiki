ff8af73375813fc94f64a9654a57cb17c81a2669
zhenxiao-yu <markyu0615@gmail.com>
2021-12-24 15:45:46 -0500

    2-7 added demo list lookup

---
+++ src/main/java/com/zhenxiao/wiki/service/DemoService.java
@@ -0,0 +1,20 @@
+package com.zhenxiao.wiki.service;
+
+import com.zhenxiao.wiki.entity.Demo;
+import com.zhenxiao.wiki.mapper.DemoMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class DemoService {
+
+    @Resource
+    private DemoMapper demoMapper;
+
+    public List<Demo> list() {
+        return demoMapper.selectByExample(null);
+    }
+
+}
