fe03e0040505dc7572771a0175b755b0dc3afda4
zhenxiao-yu <markyu0615@gmail.com>
2021-12-23 21:00:19 -0500

    1-8 hello world + basic controller test

--- src/main/java/com/zhenxiao/wiki/controller/TestController.java
+++ src/main/java/com/zhenxiao/wiki/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.zhenxiao.wiki.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class TestController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello World";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World Post," + name;
     }
 }
