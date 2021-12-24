c41a1927901028e932e844563acdd74a196c0ec8
zhenxiao-yu <markyu0615@gmail.com>
2021-12-24 15:35:09 -0500

    2-6 initialize mybatis generator

--- doc/sql/all.sql
+++ doc/sql/all.sql
@@ -4,4 +4,15 @@ create table `test` (
     `name` varchar(50) comment 'name',
     `password` varchar(50) comment 'password',
     primary key (`id`)
-) engine=innodb default charset utf8mb4 comment='test'
+) engine=innodb default charset utf8mb4 comment='test';
+
+insert into `test` (id, name, password) values (1, 'test', 'password');
+
+drop table if exists `demo`;
+create table `demo` (
+    `id` bigint not null comment 'id',
+    `name` varchar(50) comment 'name',
+    primary key (`id`)
+) engine=innodb default charset utf8mb4 comment='test_1';
+
+insert into `demo` (id, name) values (1, 'test_1');
