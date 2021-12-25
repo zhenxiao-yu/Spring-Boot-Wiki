d3b416631f0c2cf3836a934f5268a599af02077f
zhenxiao-yu <markyu0615@gmail.com>
2021-12-25 17:45:49 -0500

    3-4 extracted footer as an it's own component

---
+++ client/src/components/the-footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    2021 ZhenXiao Yu | zyu347@uwo.ca
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+//export footer component
+export default defineComponent({
+  name: 'the-footer',
+});
+</script>
