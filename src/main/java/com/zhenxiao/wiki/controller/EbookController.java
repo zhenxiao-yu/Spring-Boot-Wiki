0ce948c73251da29cd8dcafb2abe230fd4699218
zhenxiao-yu <markyu0615@gmail.com>
2021-12-24 21:45:57 -0500

    2-10 added common response type CommonResp, all backend sockets responses will be fo the CommonResp type.

--- src/main/java/com/zhenxiao/wiki/controller/EbookController.java
+++ src/main/java/com/zhenxiao/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.zhenxiao.wiki.controller;
 
 import com.zhenxiao.wiki.entity.Ebook;
+import com.zhenxiao.wiki.response.CommonResp;
 import com.zhenxiao.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +18,10 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public List<Ebook> list() {
-        return ebookService.list();
+    public CommonResp list() {
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 }
---
+++ src/main/java/com/zhenxiao/wiki/response/CommonResp.java
@@ -0,0 +1,53 @@
+package com.zhenxiao.wiki.response;
+
+public class CommonResp<T> {
+
+    /**
+     * operation status flag
+     */
+    private boolean success = true;
+
+    /**
+     * return message
+     */
+    private String message;
+
+    /**
+     * generic data
+     */
+    private T content;
+
+    public boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getContent() {
+        return content;
+    }
+
+    public void setContent(T content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("ResponseDto{");
+        sb.append("success=").append(success);
+        sb.append(", message='").append(message).append('\'');
+        sb.append(", content=").append(content);
+        sb.append('}');
+        return sb.toString();
+    }
+}
