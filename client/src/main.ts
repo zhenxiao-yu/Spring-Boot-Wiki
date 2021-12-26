29a0d96540017c5798672b197f9435e84f357fbe
zhenxiao-yu <markyu0615@gmail.com>
2021-12-26 17:06:06 -0500

    3-13 added icons from ant design vue

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,6 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Antd from 'ant-design-vue';
+import * as Icons from '@ant-design/icons-vue';
 import 'ant-design-vue/dist/antd.css';
 
-createApp(App).use(store).use(router).use(Antd).mount('#app');
+//create application component
+const app = createApp(App);
+
+app.use(store).use(router).use(Antd).mount('#app');
+
+//using all ant design icons globally
+const icons: any = Icons;
+for (const i in icons) {
+    app.component(i, icons[i]);
+}
