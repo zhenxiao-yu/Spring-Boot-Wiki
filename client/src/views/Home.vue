ae0d4f7924c82bff20140b8ff599d458101f411a
zhenxiao-yu <markyu0615@gmail.com>
2021-12-26 16:21:16 -0500

    3-11 test using both ref and reactive

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -48,13 +48,14 @@
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
       <pre>{{ebooks}}</pre>
+      <pre>{{ebooks2}}</pre>
 
     </a-layout-content>
   </a-layout>
 </template>
 
 <script lang="ts">
-import {defineComponent, onMounted, ref} from 'vue';
+import {defineComponent, onMounted, ref, reactive, toRef} from 'vue';
 import axios from 'axios';
 
 export default defineComponent({
@@ -64,18 +65,22 @@ export default defineComponent({
     //use ref() to make ebooks a dynamic data
     const ebooks = ref();
 
+    const ebooks1 = reactive({books: []});
+
     onMounted(() => {
       //get ebooks data
       axios.get("http://localhost:8080/ebook/list?name=Java").then((res) => {
         const data = res.data;
-        ebooks.value = data.content
-            console.log(res);
+        ebooks.value = data.content;
+        ebooks1.books = data.content;
+        console.log(res);
       });
-    })
+    });
 
     //return dynamic ebooks data
     return {
-      ebooks
+      ebooks,
+      ebooks2: toRef(ebooks1, "books")
     }
   }
 });
