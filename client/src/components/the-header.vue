bd58a792321a1bbe7c26eca3635faddf3321039a
zhenxiao-yu <markyu0615@gmail.com>
2021-12-27 14:15:18 -0500

    4-w added new template for admin ebook management page

--- client/src/components/the-header.vue
+++ client/src/components/the-header.vue
@@ -6,9 +6,15 @@
         mode="horizontal"
         :style="{ lineHeight: '64px' }"
     >
-      <a-menu-item key="1">nav 1</a-menu-item>
-      <a-menu-item key="2">nav 2</a-menu-item>
-      <a-menu-item key="3">nav 3</a-menu-item>
+      <a-menu-item key="/">
+        <router-link to="/">Home</router-link>
+      </a-menu-item>
+      <a-menu-item key="/admin/ebook">
+        <router-link to="/admin/ebook">Ebooks</router-link>
+      </a-menu-item>
+      <a-menu-item key="/about">
+        <router-link to="/about">About</router-link>
+      </a-menu-item>
     </a-menu>
   </a-layout-header>
 </template>
