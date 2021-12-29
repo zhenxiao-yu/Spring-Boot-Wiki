8efc784ffa5513552086cc4da8505a359ef4ac5b
zhenxiao-yu <markyu0615@gmail.com>
2021-12-29 17:39:31 -0500

    7-1 create category table, created category classes using mybatis generator

--- doc/sql/all.sql
+++ doc/sql/all.sql
@@ -12,7 +12,7 @@ create table `ebook`
     `vote_count`   int comment 'vote count',
     primary key (`id`)
 ) engine = innodb
-  default charset utf8mb4 comment ='ebook';
+  default charset utf8mb4 comment = 'ebook';
 
 insert into `ebook` (id, name, description)
 values (1, 'Intro to GUN.js',
@@ -29,3 +29,46 @@ values (4, 'JavaScript: The Good Parts',
 insert into `ebook` (id, name, description)
 values (5, 'Scrum and XP from the Trenches',
         'This book aims to give you a head start by providing a detailed down-to-earth account of how one Swe...');
+
+drop table if exists `category`;
+create table `category`
+(
+    `id`     bigint      not null comment 'id',
+    `parent` bigint      not null default 0 comment 'parent_id',
+    `name`   varchar(50) not null comment 'name',
+    `sort`   int comment 'order',
+    primary key (`id`)
+) engine = innodb
+  default charset utf8mb4 comment = 'category';
+
+insert into `category` (id, parent, name, sort)
+values (100, 000, 'Frontend', 100);
+insert into `category` (id, parent, name, sort)
+values (101, 100, 'Vue', 101);
+insert into `category` (id, parent, name, sort)
+values (102, 100, 'HTML', 102);
+insert into `category` (id, parent, name, sort)
+values (200, 200, 'Java', 200);
+insert into `category` (id, parent, name, sort)
+values (201, 200, 'Spring Boot', 201);
+insert into `category` (id, parent, name, sort)
+values (202, 200, 'Spring Cloud', 202);
+insert into `category` (id, parent, name, sort)
+values (300, 000, 'Javascript', 300);
+insert into `category` (id, parent, name, sort)
+values (301, 300, 'Node.js', 301);
+insert into `category` (id, parent, name, sort)
+values (302, 300, 'Gun.js', 302);
+insert into `category` (id, parent, name, sort)
+values (400, 000, 'Database', 400);
+insert into `category` (id, parent, name, sort)
+values (401, 400, 'MySQL', 401);
+insert into `category` (id, parent, name, sort)
+values (500, 000, 'Other', 500);
+insert into `category` (id, parent, name, sort)
+values (501, 500, 'IDE', 501);
+insert into `category` (id, parent, name, sort)
+values (502, 500, 'Game Dev', 502);
+insert into `category` (id, parent, name, sort)
+values (503, 500, 'Tools', 503);
+
---
+++ src/main/java/com/zhenxiao/wiki/mapper/CategoryMapper.java
@@ -0,0 +1,31 @@
+package com.zhenxiao.wiki.mapper;
+
+import com.zhenxiao.wiki.entity.Category;
+import com.zhenxiao.wiki.entity.CategoryExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface CategoryMapper {
+    long countByExample(CategoryExample example);
+
+    int deleteByExample(CategoryExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Category record);
+
+    int insertSelective(Category record);
+
+    List<Category> selectByExample(CategoryExample example);
+
+    Category selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Category record, @Param("example") CategoryExample example);
+
+    int updateByExample(@Param("record") Category record, @Param("example") CategoryExample example);
+
+    int updateByPrimaryKeySelective(Category record);
+
+    int updateByPrimaryKey(Category record);
+}
