7057727ca5641af05acaf522f1630ce4efe21f3e
zhenxiao-yu <markyu0615@gmail.com>
2021-12-30 16:18:35 -0500

    7-8 show category on sider

--- client/src/views/home.vue
+++ client/src/views/home.vue
@@ -1,53 +1,42 @@
 <template>
   <a-layout>
+    <!--  SIDE MENU  -->
     <a-layout-sider width="200" style="background: #fff">
       <a-menu
           mode="inline"
-          v-model:openKeys="openKeys"
           :style="{ height: '100%', borderRight: 0 }"
+          @click="handleClick"
+          :openKeys="openKeys"
       >
-        <a-sub-menu key="sub1">
-          <template #title>
-              <span>
-                <user-outlined/>
-                subnav 1
-              </span>
+        <!--  link to show welcome component -->
+        <a-menu-item key="welcome">
+          <MailOutlined/>
+          <span>Welcome</span>
+        </a-menu-item>
+        <!-- show parent categories -->
+        <a-sub-menu v-for="item in level1" :key="item.id" :disabled="true">
+          <template v-slot:title>
+            <span><user-outlined/>{{ item.name }}</span>
           </template>
-          <a-menu-item key="1">option1</a-menu-item>
-          <a-menu-item key="2">option2</a-menu-item>
-          <a-menu-item key="3">option3</a-menu-item>
-          <a-menu-item key="4">option4</a-menu-item>
+          <a-menu-item v-for="child in item.children" :key="child.id">
+            <MailOutlined/>
+            <span> {{ child.name }} </span>
+          </a-menu-item>
         </a-sub-menu>
-        <a-sub-menu key="sub2">
-          <template #title>
-              <span>
-                <laptop-outlined/>
-                subnav 2
-              </span>
-          </template>
-          <a-menu-item key="5">option5</a-menu-item>
-          <a-menu-item key="6">option6</a-menu-item>
-          <a-menu-item key="7">option7</a-menu-item>
-          <a-menu-item key="8">option8</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub3">
-          <template #title>
-              <span>
-                <notification-outlined/>
-                subnav 3
-              </span>
-          </template>
-          <a-menu-item key="9">option9</a-menu-item>
-          <a-menu-item key="10">option10</a-menu-item>
-          <a-menu-item key="11">option11</a-menu-item>
-          <a-menu-item key="12">option12</a-menu-item>
-        </a-sub-menu>
+
+        <a-menu-item key="tip" :disabled="true">
+          <span>Configure In Categories</span>
+        </a-menu-item>
       </a-menu>
     </a-layout-sider>
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      <a-list item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3 }" :data-source="ebooks">
+      <div class="welcome" v-show="isShowWelcome">
+        <the-welcome></the-welcome>
+      </div>
+      <a-list v-show="!isShowWelcome" item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3 }"
+              :data-source="ebooks">
         <template #renderItem="{ item }">
           <a-list-item key="item.name">
             <template #actions>
@@ -78,8 +67,6 @@
           </a-list-item>
         </template>
       </a-list>
-
-
     </a-layout-content>
   </a-layout>
 </template>
@@ -88,33 +75,97 @@
 import {defineComponent, onMounted, ref, reactive, toRef} from 'vue';
 import axios from 'axios';
 import {message} from 'ant-design-vue';
+import {Tool} from "@/util/tool";
+import TheWelcome from '@/components/the-welcome.vue';
+
 
 export default defineComponent({
   name: 'Home',
+  components: {
+    TheWelcome
+  },
   setup() {
     //use ref() to make ebooks a dynamic data
     const ebooks = ref();
     //using reactive
     // const ebooks1 = reactive({books: []});
 
-    onMounted(() => {
-      //get ebooks data
+    const openKeys = ref();
+
+    const level1 = ref();
+
+    let categorys: any;
+
+    // return list of all categories
+    const handleQueryCategory = () => {
+      axios.get("/category/all").then((response) => {
+        const data = response.data;
+        if (data.success) {
+          categorys = data.content;
+          console.log("Original：", categorys);
+          //open child categories when loading finishes
+          openKeys.value = [];
+          for (let i = 0; i < categorys.length; i++) {
+            openKeys.value.push(categorys[i].id)
+          }
+
+          level1.value = [];
+          level1.value = Tool.array2Tree(categorys, 0);
+          console.log("Tree：", level1.value);
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
+    const isShowWelcome = ref(true);
+    let categoryId2 = 0;
+
+    const handleQueryEbook = () => {
       axios.get("/ebook/list", {
         params: {
-          page: 1,
-          size: 1000
+          page: 1, //1 page
+          size: 1000, //max 1000 ebooks per page
+          categoryId2: categoryId2
         }
-      }).then((res) => {
-        const data = res.data;
+      }).then((response) => {
+        const data = response.data;
         ebooks.value = data.content.list;
         // ebooks1.books = data.content;
       });
+    };
+
+    const handleClick = (value: any) => {
+      // console.log("menu click", value)
+      if (value.key === 'welcome') {
+        isShowWelcome.value = true;
+      } else {
+        categoryId2 = value.key;
+        isShowWelcome.value = false;
+        handleQueryEbook();
+      }
+      // isShowWelcome.value = value.key === 'welcome';
+    };
+
+    onMounted(() => {
+      handleQueryCategory();
     });
 
     //return dynamic ebooks data
     return {
       ebooks,
       // ebooks2: toRef(ebooks1, "books")
+      // listData,
+      pagination: {
+        onChange: (page: any) => {
+          console.log(page);
+        },
+        pageSize: 3,
+      },
+      handleClick,
+      level1,
+      isShowWelcome,
+      openKeys
     }
   }
 });
@@ -122,11 +173,11 @@ export default defineComponent({
 
 <!--style for current component-->
 <style scoped>
-.ant-avatar {
-  width: 50px;
-  height: 50px;
-  line-height: 50px;
-  border-radius: 8%;
-  margin: 5px 0;
-}
+  .ant-avatar {
+    width: 50px;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 8%;
+    margin: 5px 0;
+  }
 </style>
